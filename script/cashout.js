20415566484ba8039510cecb97ce6ee531e03c14
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-17 10:46:23 -0400

    add cashout layout in homepage

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener('click', function(){
+    // 1- get the agent number && validate
+    // 2- get the amount, validate, convert to number
+    // 3- get current balance, validate, convert to number
+    // 4- calculate new balance
+    // 5- get the pin and verify
+    // 5-1 true:: show an alert > set balance
+    // 5-2 false:: show an error alert > return
+});
