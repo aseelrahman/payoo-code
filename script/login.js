e6ded25753bcc5da8adb8b69e891e7b9d2a5a951
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-17 09:26:47 -0400

    complete login page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,26 @@
-console.log("login functionality coming");
+document.getElementById('login-btn').addEventListener('click',
+    function(event){
+        // 1- get the mobile number
+        const numberInput = document.getElementById('input-number');
+        const contactNumber = numberInput.value;
+
+        // 2- get the pin
+        const inputPin = document.getElementById('input-pin');
+        const pin = inputPin.value;
+
+        // 3- match the pin and number
+        if(contactNumber == '01234567890' && pin == '1234'){
+            // 3-1 true ----> alert > homepage
+            alert("Login Success");
+
+            // window.location.replace("/home.html")
+            window.location.assign("/home.html");
+        }
+        else{
+            // 3-2 false ---> alert > return
+            alert("Login Failed");
+            return;
+        }
+        
+    }
+)
