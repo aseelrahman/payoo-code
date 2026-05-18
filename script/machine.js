72a673f2e95017e51cf4165c6bbd8c76e6c42a57
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-18 09:52:47 -0400

    Complete Payoo Project

--- script/machine.js
+++ script/machine.js
@@ -17,14 +17,16 @@ function showOnly(id){
     const addMoney = document.getElementById("add-money");
     const cashout = document.getElementById("cashout");
     const transactions = document.getElementById("history");
-    const sendMoney = document.getElementById("sendMoney")
-    const getBonus = document.getElementById("getBonus")
+    const sendMoney = document.getElementById("sendMoney");
+    const getBonus = document.getElementById("getBonus");
+    const payBill = document.getElementById("pay-bill");
 
     addMoney.classList.add("hidden");
     cashout.classList.add("hidden");
-    transactions.classList.add("hidden")
-    sendMoney.classList.add("hidden")
-    getBonus.classList.add("hidden")
+    transactions.classList.add("hidden");
+    sendMoney.classList.add("hidden");
+    getBonus.classList.add("hidden");
+    payBill.classList.add("hidden")
  
     document.getElementById(id).classList.remove("hidden")
 
